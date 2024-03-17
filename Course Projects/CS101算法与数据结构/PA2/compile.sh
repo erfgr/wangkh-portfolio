ec8ae981b92de8330c67848486c1a49a4d304186
Fourier <93697931+erfgr@users.noreply.github.com>
2024-03-17 19:10:10 +0800

    Delete Course Projects/CS101算法与数据结构 directory

--- Course Projects/CS101算法与数据结构/PA2/compile.sh
+++
@@ -1,4 +0,0 @@
-# for release mode
-g++ -Wall -Wextra -std=c++17                                           -O2 dsa.cc main.cc -o dsa-sort
-# for debug mode
-# g++ -Wall -Wextra -std=c++17 -g -fsanitize=undefined -fsanitize=address    dsa.cc main.cc -o dsa-sort
--- Course Projects/CS101算法与数据结构/PA3/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ -Wall -Wextra -std=c++17 -O2 dsa.cc main.cc -o test
