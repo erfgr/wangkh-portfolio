1d2684559c65940771fe2f088996cf6059d82a3e
Fourier <93697931+erfgr@users.noreply.github.com>
2024-03-17 19:04:32 +0800

    Delete Course Projects/CS110计算机体系结构（CA） directory

--- Course Projects/CS110计算机体系结构（CA）/p1.2_liuyx6_wangkh/riscv.h
+++
@@ -1,9 +0,0 @@
-#ifndef RISCV_H
-#define RISCV_H
-
-#include "part1/assembler.h"
-#include "part2/emulator.h"
-#include "types.h"
-#include "utils.h"
-
-#endif
--- Course Projects/CS110计算机体系结构（CA）/p2.2_liuyx6_wangkh/tests/input/mulh.s
+++
@@ -1,3 +0,0 @@
-lui t0 0xFFFF0
-lui t1 0x10
-mulh t2, t0, t1
